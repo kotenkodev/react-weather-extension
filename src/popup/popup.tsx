6df9f18308bd56f3ab4cdac37cad4c4e4997222a
Vadym <vadym.kotenko.dev@gmail.com>
2026-03-31 00:22:44 +0300

    feat: enhance cities list cards

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './popup.css';
 import WeatherCard from '../components/WeatherCard';
+import { InputBase, IconButton, Paper, Box, Grid } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
-const App: React.FC<{}> = () => {
-    return (
-        <div>
-            <WeatherCard city="London" />
-            <WeatherCard city="Toronto" />
-            <WeatherCard city="Error" />
-            <img src="icon.png" />
-        </div>
-    );
-};
+function App() {
+  const [cities, setCities] = useState<string[]>(['Toronto', 'London', 'Error']);
+  const [cityInput, setCityInput] = useState<string>('');
+
+  const handleCityInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCityInput(event.target.value);
+  };
+
+  return (
+    <Box mx={'8px'} my={'16px'}>
+      <Grid container>
+        <Paper>
+          <Box px={'15px'} py={'5px'}>
+            <InputBase placeholder="Add a city name" value={cityInput} onChange={handleCityInput} />
+            <IconButton>
+              <AddIcon />
+            </IconButton>
+          </Box>
+        </Paper>
+      </Grid>
+      {cities.map((city, index) => (
+        <WeatherCard key={`${city}-${index}`} city={city} />
+      ))}
+      <img src="icon.png" />
+    </Box>
+  );
+}
 
 const container = document.createElement('div');
 document.body.appendChild(container);
