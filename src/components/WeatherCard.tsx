e483b857e3d354d7cb4b7b47790ee8c7291fcf82
Vadym <vadym.kotenko.dev@gmail.com>
2026-03-31 00:08:54 +0300

    feat: add fallbacks after fetching weather data

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -2,41 +2,48 @@ import { Box, Card, CardContent, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { fetchOpenWeatherData, WeatherData } from '../api/weather';
 
+export const WeatherCardContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Box mx={'4px'} my={'16px'}>
+      <Card>
+        <CardContent>{children}</CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+type WeatherCardState = 'loading' | 'ready' | 'error';
+
 export default function WeatherCard({ city }: { city: string }) {
-    const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [weatherState, setWeatherState] = useState<WeatherCardState>('loading');
 
-    useEffect(() => {
-        fetchOpenWeatherData(city)
-            .then((data) => {
-                setWeatherData(data);
-                console.log(data);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }, []);
+  useEffect(() => {
+    fetchOpenWeatherData(city)
+      .then((data) => {
+        setWeatherData(data);
+        setWeatherState('ready');
+      })
+      .catch((error) => {
+        setWeatherState('error');
+      });
+  }, []);
 
-    if (!weatherData) {
-        return (
-            <Card>
-                <CardContent>
-                    <Typography variant="h5">Loading...</Typography>
-                </CardContent>
-            </Card>
-        );
-    }
-
+  if (weatherState == 'error' || weatherState == 'loading') {
     return (
-        <Box mx={'4px'} my={'16px'}>
-            <Card>
-                <CardContent>
-                    <Typography variant="h5">{city}</Typography>
-                    <Typography variant="body1">{Math.round(weatherData.main.temp)}</Typography>
-                    <Typography variant="body1">
-                        Feels like: {Math.round(weatherData.main.feels_like)}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </Box>
+      <WeatherCardContainer>
+        <Typography variant="h5">
+          {weatherState == 'loading' ? 'Loading...' : 'Error loading weather data for this city.'}
+        </Typography>
+      </WeatherCardContainer>
     );
+  }
+
+  return (
+    <WeatherCardContainer>
+      <Typography variant="h5">{city}</Typography>
+      <Typography variant="body1">{Math.round(weatherData.main.temp)}</Typography>
+      <Typography variant="body1">Feels like: {Math.round(weatherData.main.feels_like)}</Typography>
+    </WeatherCardContainer>
+  );
 }
