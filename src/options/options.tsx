0503940e76bcd7121a1cdac4a294a85853717cf2
Vadym <vadym.kotenko.dev@gmail.com>
2026-03-31 02:54:16 +0300

    feat: add options page

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './options.css';
+import { Box, Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material';
+import { getStoredOptions, LocalStorageOptions, setStoredOptions } from '../utils/storage';
 
 const App = () => {
+  const [options, setOptions] = useState<LocalStorageOptions | null>(null);
+
+  useEffect(() => {
+    getStoredOptions().then((options) => {
+      setOptions(options);
+    });
+  }, []);
+
+  const handleHomeCityChange = (homeCity: string) => {
+    setOptions({
+      ...options,
+      homeCity,
+    });
+  };
+
+  const handleSave = () => {
+    console.log(options);
+    setStoredOptions(options);
+  };
+
+  if (!options) {
+    return null;
+  }
+
   return (
-    <div>
-      <img src="icon.png" />
-    </div>
+    <Box mx={'10%'} my={'2%'}>
+      <Card>
+        <CardContent>
+          <Grid spacing={4} container direction={'column'}>
+            <Grid>
+              <Typography variant="h4">Weather Extension Options</Typography>
+            </Grid>
+            <Grid>
+              <Typography variant="body1">Home city name:</Typography>
+              <TextField
+                onChange={(e) => handleHomeCityChange(e.target.value)}
+                value={options.homeCity}
+                fullWidth
+                placeholder="Enter a home city name"
+              />
+            </Grid>
+            <Grid>
+              <Button onClick={handleSave} variant="contained" color="primary">
+                Save
+              </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
